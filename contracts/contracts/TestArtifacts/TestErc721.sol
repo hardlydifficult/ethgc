b229668ba04dba876753959e23c9a21dc13fa7fa
hardlydifficult <hardlydifficultllc@gmail.com>
2019-03-19 15:14:09 -0700

    Test NftProxy

--- contracts/contracts/TestArtifacts/TestErc721.sol
+++ contracts/contracts/TestArtifacts/TestErc721.sol
@@ -3,4 +3,4 @@ pragma solidity ^0.5.6;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
 
 
-contract ExampleErc721 is ERC721Mintable {}
+contract TestErc721 is ERC721Mintable {}
