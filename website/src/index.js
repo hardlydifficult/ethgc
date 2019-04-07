3dcf4547fe9c5464454880b0527a7270a9a5d64c
hardlydifficult <hardlydifficultllc@gmail.com>
2019-04-07 10:03:17 -0700

    webpack fix (?)

--- website/src/index.js
+++ website/src/index.js
@@ -1,3 +1,7 @@
+import "babel-polyfill";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
@@ -7,8 +11,7 @@ import VTooltip from "v-tooltip";
 import Clipboard from "v-clipboard";
 import Ethjs from "../../library/ethgc.js";
 import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import router from "./router";
 Vue.use(BootstrapVue);
 Vue.use(AsyncComputed);
 Vue.use(VTooltip);
@@ -24,8 +27,13 @@ if (window.ethereum) {
 } // TODO maybe else means go get metamask
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: "#app",
+//   components: { App },
+//   template: "<App/>"
+// });
+
 new Vue({
-  el: "#app",
-  components: { App },
-  template: "<App/>"
-});
+  router,
+  render: h => h(App)
+}).$mount("#app");
