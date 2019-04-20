f5d04b5238367d061312b6c18e8d95bf1cce7f25
hardlydifficult <hardlydiff@gmail.com>
2019-04-19 17:58:08 -0700

    Adding an etherscan link to the tx history

--- website/src/components/TxHistory/index.vue
+++ website/src/components/TxHistory/index.vue
@@ -2,9 +2,15 @@
   <div v-on:tx="onTx">
     Transaction history:
     <ul class="list-group" v-if="transactions.length">
-      <li class="list-group-item" v-for="(tx, index) in transactions" :key="index">
-          <StatusIcon :status="{url: '0x86c74643e51183b739c3f2164455ec6ef5077f9c037d4c657764a77c3470aab1', urlMessage: 'etherscan.com'}" />
-          <!-- <StatusIcon :status="{loadingMessage:'wip<br><small>click to view on EtherScan</small>'}" /> -->
+      <li
+        class="list-group-item"
+        v-for="(tx, index) in transactions"
+        :key="index"
+      >
+        <ViewOnEtherscan :tx="tx" />
+
+        <!-- <StatusIcon :status="{loadingMessage:'wip<br><small>click to view on EtherScan</small>'}" /> -->
+
         Create card
         <Date :date="Date.now()" />
         <span v-tooltip="'Success - 5 confirmations. Mined 20 mins ago'">
@@ -22,25 +28,27 @@
 <script>
 import Date from "../Widgets/Date";
 import StatusIcon from "../Widgets/StatusIcon";
+import ViewOnEtherscan from "../Widgets/ViewOnEtherscan";
 
 export default {
   components: {
     Date,
-    StatusIcon
+    StatusIcon,
+    ViewOnEtherscan
   },
   data() {
     return {
       transactions: []
     };
   },
-  mounted () {
-    this.transactions = this.$ls.get("transactions") || []
+  mounted() {
+    this.transactions = this.$ls.get("transactions") || [];
     this.$observable.subscribe("tx", this.onTx);
   },
   methods: {
     onTx(tx) {
-      this.transactions.push(tx)
-      this.$ls.set("transactions", this.transactions)
+      this.transactions.push(tx);
+      this.$ls.set("transactions", this.transactions);
     }
   }
 };
