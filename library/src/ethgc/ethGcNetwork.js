b034066cd83580cb8030a41b0bc816a236736b1e
NickCuso <hardlydiff@gmail.com>
2020-04-22 13:29:06 -0700

    Remove logs

--- library/src/ethgc/ethGcNetwork.js
+++ library/src/ethgc/ethGcNetwork.js
@@ -37,21 +37,12 @@ class EthGcNetwork {
   ) {
     await this._init()
     this.parseInput(tokenAddresses, valueOrIds)
-    console.log({cardAddresses,
-      tokenAddresses,
-      valueOrIds})
     let ethValue = await this.calcEthRequired(
       cardAddresses,
       tokenAddresses,
       valueOrIds,
       true
     )
-
-    console.log({cardAddresses,
-      tokenAddresses,
-      valueOrIds,
-      description,
-      redeemedMessage, ethValue})
 
     return this.hardlyWeb3.send(
       this.contract.methods.create(
@@ -138,16 +129,9 @@ class EthGcNetwork {
     await this._init()
     this.parseInput(tokenAddresses, valueOrIds)
 
-    console.log('-----------------')
-    console.log({cardAddresses, tokenAddresses, valueOrIds, isNewCard})
-
     const redemptionGas = await this.extContract.methods
       .getFees(cardAddresses, tokenAddresses, valueOrIds, isNewCard)
       .call({ from: this.hardlyWeb3.defaultAccount() })
-
-    console.log('-----------------')
-    console.log('-----------------')
-    console.log('-----------------')
 
     return new BigNumber(redemptionGas)
   }
