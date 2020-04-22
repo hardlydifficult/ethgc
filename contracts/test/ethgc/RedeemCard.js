f35e8d10778ef18855f19cccf0d09abc1880a057
Hardly Difficult <hardlydiff@gmail.com>
2020-04-21 20:17:55 -0700

    Remove shouldFail (#952)
    * Remove shouldFail
    
    * Update Ethgc.sol

--- contracts/test/ethgc/RedeemCard.js
+++ contracts/test/ethgc/RedeemCard.js
@@ -1,10 +1,10 @@
 const init = require('./helpers/init')
-const shouldFail = require('../helpers/shouldFail')
+const { reverts } = require('truffle-assertions')
 
 contract('RedeemCard', accounts => {
   let ethgc
 
-  before(async () => {
+  beforeEach(async () => {
     ethgc = await init(accounts)
   })
 
@@ -12,24 +12,29 @@ contract('RedeemCard', accounts => {
     const redeemCode = 'abc123'
     const value = 42
 
-    before(async () => {
+    beforeEach(async () => {
       const cardAddress = await ethgc.getCardAddress(redeemCode)
-
       await ethgc.create([cardAddress], [web3.utils.padLeft(0, 40)], [value])
     })
 
-    it('Can redeem', async () => {
-      const balance = await ethgc.hardlyWeb3.getEthBalance()
-      await ethgc.redeem(redeemCode)
-      assert.equal(
-        (await ethgc.hardlyWeb3.getEthBalance()).toFixed(),
-        balance.plus(value).toFixed()
-      )
-    })
+    describe('after redeem', async () => {
+      let balanceBefore
 
-    it.skip('shouldFail to claim a claimed code', async () => {
-      // not sure why shouldFail helper is not working ATM.
-      await shouldFail(ethgc.redeem(redeemCode), 'ALREADY_CLAIMED')
+      beforeEach(async () => {
+        balanceBefore = await ethgc.hardlyWeb3.getEthBalance()
+        await ethgc.redeem(redeemCode)
+      })
+
+      it('Balance went up', async () => {
+        assert.equal(
+          (await ethgc.hardlyWeb3.getEthBalance()).toFixed(),
+          balanceBefore.plus(value).toFixed()
+        )
+      })
+
+      it('shouldFail to claim a claimed code', async () => {
+        await reverts(ethgc.redeem(redeemCode), 'ALREADY_CLAIMED')
+      })
     })
   })
 })
