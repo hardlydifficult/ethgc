04c896c420b9e207698849086df7a15664306b79
NickCuso <hardlydiff@gmail.com>
2020-04-28 12:15:44 -0700

    Revert website

---
+++ website.new/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <transition mode="out-in"
+                name="fade">
+      <router-view />
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss" src="@/assets/scss/style.scss"></style>
